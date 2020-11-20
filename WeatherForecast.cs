885d0167ad5cdf17687310b8c322427c2e54a5c9
Vivien FABING <vfabing@live.com>
2020-11-20 13:39:40 +0100

    dotnet new react

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace simple_aspnetcore_react_shared_generic_errors
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
